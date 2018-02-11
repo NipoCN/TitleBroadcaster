1ed268ab5f1e219ccc8785526bf9f51f732be56a
Nipo_CN <Nipo@outlook.com>
2018-02-12 00:24:20 +0800

    Add files via upload

---
+++ src/xin/omen/titlebroadcaster/TitleBroadcaster.java
@@ -0,0 +1,39 @@
+package xin.omen.titlebroadcaster;
+
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameAboutToStartServerEvent;
+import org.spongepowered.api.command.args.GenericArguments;
+import org.slf4j.Logger;
+import com.google.inject.Inject;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.text.Text;
+
+import xin.omen.titlebroadcaster.commands.CommandMain;
+import org.spongepowered.api.Game;
+
+@Plugin(id="titlebroadcaster", name="TitleBroadcaster", version="1.0.0-SNAPSHOT", authors="NipoCN")
+public class TitleBroadcaster {
+	
+	@Inject
+	private Game game;
+	
+	@Inject
+	private Logger logger;
+	
+	private static TitleBroadcaster instance;
+	
+	@Listener
+	public void onGameAboutToStartServerEvent(GameAboutToStartServerEvent event){
+		instance = this;
+		CommandSpec mainCommand = CommandSpec.builder()
+				.description(Text.of("TitleBroadcaster Main Command"))
+				.arguments(
+						GenericArguments.onlyOne(GenericArguments.string(Text.of("message")))
+				)
+				.executor(new CommandMain())
+				.build();
+		game.getCommandManager().register(this, mainCommand, "titlebroadcaster", "tb");
+		logger.info("Plugin Initialized, thanks for using TitleBroadcaster by NipoCN");
+	}
+}
---
+++ src/xin/omen/titlebroadcaster/commands/CommandMain.java
@@ -0,0 +1,48 @@
+package xin.omen.titlebroadcaster.commands;
+
+import org.spongepowered.api.command.*;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.text.*;
+import org.spongepowered.api.text.format.TextColors;
+import org.spongepowered.api.entity.living.player.*;
+import org.spongepowered.api.text.title.*;
+import org.spongepowered.api.Sponge;
+
+public class CommandMain implements CommandExecutor {
+	
+	@Override
+	public CommandResult execute (CommandSource Sender, CommandContext args) throws CommandException {
+		if (Sender instanceof Player == false){
+			String message = args.<String>getOne("message").get();
+			Title send = Title.builder()
+					.title(Text.of(TextColors.GOLD, message))
+					.subtitle(Text.of(TextColors.RED, "- Server"))
+					.fadeIn(10)
+					.stay(60)
+					.fadeOut(10)
+					.build();
+			for (Player OnlinePlayer : Sponge.getServer().getOnlinePlayers()){
+				OnlinePlayer.sendTitle(send);
+			}
+			return CommandResult.success();
+		}
+		if (!Sender.hasPermission("xin.omen.titlebroadcaster.use")){
+			Sender.sendMessage(Text.of(TextColors.RED, "You don't have permission set use this command"));
+			return CommandResult.success();
+		}
+		Player player = (Player) Sender;
+		String message = args.<String>getOne("message").get();
+		Title send = Title.builder()
+				.title(Text.of(TextColors.GOLD, message))
+				.subtitle(Text.of(TextColors.BLUE, "- From ", player.getName()))
+				.fadeIn(10)
+				.stay(60)
+				.fadeOut(10)
+				.build();
+		for (Player OnlinePlayer : Sponge.getServer().getOnlinePlayers()){
+			OnlinePlayer.sendTitle(send);
+		}
+		return CommandResult.success();
+	}
+}
